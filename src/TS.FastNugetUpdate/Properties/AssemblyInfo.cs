3c255c92589bb79cf2674bd69e9eddb2b183b04e
AndreyTsvetkov <ts.andrey@gmail.com>
2016-09-14 14:36:17 +0300

    project created

---
+++ src/TS.FastNugetUpdate/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TS.FastNugetUpdate")]
+[assembly: AssemblyDescription("Updates nuget references")]
+[assembly: AssemblyCompany("TS")]
+[assembly: AssemblyProduct("TS.FastNugetUpdate")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c74b89d7-2d95-4b05-96a9-2b39ff5569d6")]
+
+[assembly: AssemblyVersion("0.0.1")]
+
