f06fb6b58ec1a250792b1ca66a887c319521640b
AndreyTsvetkov <ts.andrey@gmail.com>
2016-09-21 09:18:19 +0300

    Support for the pre-release packages #3

--- src/TS.FastNugetUpdate/NugetUpdate.cs
+++ src/TS.FastNugetUpdate/NugetUpdate.cs
@@ -78,7 +78,7 @@ namespace TS.FastNugetUpdate
 			var reference = new Regex(
 				$@"<Reference Include=""{RegexEncode(name)}, Version=[^""]+"">");
 			var hintPath = new Regex(
-				$@"<HintPath>(?<packageRoot>[\.\w\\]*?packages)\\{RegexEncode(name)}\.\d+\.\d+\.\d+(\.\d+)?\\(?<restPath>[^<]+)</HintPath>");
+				$@"<HintPath>(?<packageRoot>[\.\w\\]*?packages)\\{RegexEncode(name)}\..*?\\(?<restPath>[^<]+)</HintPath>");
 
 			return content =>
 			{
